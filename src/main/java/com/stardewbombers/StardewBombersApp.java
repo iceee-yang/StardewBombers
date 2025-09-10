73d189ae8062752960b272117dd8b733c8c1ed95
Leecream <2523967486@qq.com>
2025-09-10 10:29:11 +0800

    合并部分代码

--- src/main/java/com/stardewbombers/StardewBombersApp.java
+++ src/main/java/com/stardewbombers/StardewBombersApp.java
@@ -1,16 +1,29 @@
 package com.stardewbombers;
 
+import com.stardewbombers.client.ui.MainMenuView;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class StardewBombersApp extends Application {
-	public static void main(String[] args) {
-		launch(args);
-	}
 
 	@Override
 	public void start(Stage stage) {
+		MainMenuView view = new MainMenuView();
+		view.setOnStart(() -> System.out.println("开始游戏被点击"));
+		view.setOnSettings(() -> System.out.println("设置被点击"));
+		view.setOnExit(() -> {
+			System.out.println("退出被点击");
+			stage.close();
+		});
+
+		Scene scene = new Scene(view, 960, 540);
 		stage.setTitle("Stardew Bombers");
+		stage.setScene(scene);
 		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
